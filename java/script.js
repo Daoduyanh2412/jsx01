1805f18fd4a8b5e21c8b70d0abe76b63e4790fb7
Đào Duy Anh <131458303+Daoduyanh2412@users.noreply.github.com>
2023-05-06 20:35:12 +0700

    Update all

---
+++ java/script.js
@@ -0,0 +1,21 @@
+
+const a=9;
+if(a>=8){
+    console.log("Học sinh giỏi");
+        if(a==10){
+            console.log("Qúa giỏi");
+        }
+        else{
+            console.log("Còn non lắm em ơi");
+        }
+}
+else if(6.5<=a<=7.9){
+    console.log("Học sinh Khá");
+}
+else if(a<5){
+    console.log("Học sinh trung bình");
+}
+else{
+    console.log("Học sinh kém");
+}
+
